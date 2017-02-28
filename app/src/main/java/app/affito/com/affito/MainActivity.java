6561bb1733768c6c18277723a0a805755ad55d15
Sarvesh Palav <sarveshpalav@Sarveshs-MacBook-Air.local>
2017-02-28 13:47:00 +0530

    Initial

---
+++ app/src/main/java/app/affito/com/affito/MainActivity.java
@@ -0,0 +1,13 @@
+package app.affito.com.affito;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
